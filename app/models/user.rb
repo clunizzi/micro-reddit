f96441b3478aa8f9e424fa252face6f36762d2c2
Andrea Tozzini <a.tozzini@gmail.com>
2015-11-26 11:47:45 +0000

    Remove password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
+    validates :password, presence: true, length: { minimum: 4 }
 end
